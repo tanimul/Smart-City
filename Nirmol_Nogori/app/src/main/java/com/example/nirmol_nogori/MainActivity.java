f0d60af275c69317e439a6030faa39e21d818d7a
Fuad Hasan <fuadhasanfhm@gmail.com>
2020-01-23 13:18:12 +0600

    first commit

---
+++ Nirmol_Nogori/app/src/main/java/com/example/nirmol_nogori/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nirmol_nogori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
