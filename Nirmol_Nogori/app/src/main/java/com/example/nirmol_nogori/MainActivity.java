29e06915fcbd07989bafb82242b8b3aeda610228
Fuad <fuadhasanfhm@gmail.com>
2020-02-16 21:23:55 +0600

    add google and facebook login

--- Nirmol_Nogori/app/src/main/java/com/example/nirmol_nogori/MainActivity.java
+++ Nirmol_Nogori/app/src/main/java/com/example/nirmol_nogori/MainActivity.java
@@ -1,38 +1,320 @@
 package com.example.nirmol_nogori;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.squareup.picasso.Picasso;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button button_go_login;
+
+    private Button button_go_login, button_signout;
     private TextView textView_go_registration;
+    private SignInButton signInButton_google;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private int RC_sign_in = 1;
+    private long backpressed;
+    private Toast backtost;
+    private CallbackManager callbackManager;
+    private LoginButton login_facebook;
+    private AccessTokenTracker accessTokenTracker;
+    private static final String TAG = "fff";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button_go_login = findViewById(R.id.button_go_Login);
         textView_go_registration = findViewById(R.id.textView_from_main_registration);
+        signInButton_google = findViewById(R.id.signin_google);
+        button_signout = findViewById(R.id.button_signout);
+        login_facebook = findViewById(R.id.login_facebook_button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        login_facebook.setReadPermissions("email", "public_profile");
+        callbackManager = CallbackManager.Factory.create();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
         button_go_login.setOnClickListener(this);
         textView_go_registration.setOnClickListener(this);
-
+        button_signout.setOnClickListener(this);
+        signInButton_google.setOnClickListener(this);
+        login_facebook.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
+
         if (R.id.button_go_Login == v.getId()) {
             Intent intent = new Intent(MainActivity.this, Login.class);
             startActivity(intent);
-        } else {
+        } else if (R.id.textView_from_main_registration == v.getId()) {
             Intent intent = new Intent(MainActivity.this, Registration.class);
             startActivity(intent);
+        } else if (R.id.button_signout == v.getId()) {
+            signOut();
+        } else if (R.id.signin_google == v.getId()) {
+            signIn_google();
+        } else if (R.id.login_facebook_button == v.getId()) {
+            signIn_facebook();
         }
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_sign_in) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSigninResult(task);
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+
+    private void updateUI(FirebaseUser firebaseUser) {
+
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+
+        if (account != null) {
+            button_signout.setVisibility(View.VISIBLE);
+            String personname = account.getDisplayName();
+            String personemail = account.getEmail();
+            String personid = account.getId();
+            Toast.makeText(this, personemail + "\n" + personname + "\n" + personid, Toast.LENGTH_SHORT).show();
+        }
+        if (firebaseUser != null) {
+            Toast.makeText(this, "You're logged in", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "You're not logged in", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // firebaseAuth.addAuthStateListener(authStateListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            //  firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    private void signIn_google() {
+
+        Intent signin_intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signin_intent, RC_sign_in);
+
+    }
+
+    private void signIn_facebook() {
+
+        login_facebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handlefac_SigninResult(loginResult.getAccessToken());
+                Toast.makeText(MainActivity.this, "on sunccess", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "on cancel", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "myerror" + error);
+                Toast.makeText(MainActivity.this, "on error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+                if (currentUser != null) {
+                    updateUI(currentUser);
+                } else {
+                    updateUI(null);
+                }
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (accessTokenTracker == null) {
+                    firebaseAuth.signOut();
+                }
+            }
+        };
+
+    }
+
+    private void signOut() {
+
+        googleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        firebaseAuth.signOut();
+                        button_signout.setVisibility(View.INVISIBLE);
+                    }
+                });
+
+    }
+
+    private void handleSigninResult(Task<GoogleSignInAccount> completedtask) {
+
+        try {
+
+            GoogleSignInAccount acc = completedtask.getResult(ApiException.class);
+            Toast.makeText(this, "Signed in successfully", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(acc);
+
+        } catch (ApiException e) {
+
+            Toast.makeText(this, "Sign in Failed", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(null);
+
+        }
+
+    }
+
+    private void handlefac_SigninResult(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_SHORT).show();
+                    updateUI(user);
+                } else {
+                    Toast.makeText(MainActivity.this, "failed ", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    firebaseAuth.signOut();
+                }
+            }
+        };
+    }
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acct) {
+
+        try {
+
+            AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+            firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+
+                }
+            });
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (backpressed + 2000 > System.currentTimeMillis()) {
+            backtost.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backtost = Toast.makeText(MainActivity.this, "press back again to Exit", Toast.LENGTH_SHORT);
+            backtost.show();
+        }
+
+        backpressed = System.currentTimeMillis();
+
+    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//
+//        callbackManager.onActivityResult(requestCode, resultCode, data);
+//    }
+
+
 }
--- Nirmol_Nogori/app/src/main/java/com/example/nirmol_nogori/Registration.java
+++ Nirmol_Nogori/app/src/main/java/com/example/nirmol_nogori/Registration.java
@@ -1,24 +1,172 @@
 package com.example.nirmol_nogori;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Registration extends AppCompatActivity implements View.OnClickListener {
-private Button buttonregistration;
+
+    private Button buttonregistration;
+    private TextView go_login_from_Reg;
+    private EditText f_name, l_name, u_email, u_password, u_con_password;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        buttonregistration=findViewById(R.id.button_registration);
+
+        buttonregistration = findViewById(R.id.button_registration);
+        f_name = findViewById(R.id.edittext_firstname);
+        l_name = findViewById(R.id.edittext_lastname);
+        u_email = findViewById(R.id.edittext_registration_email);
+        u_password = findViewById(R.id.edittext_registration_password);
+        u_con_password = findViewById(R.id.edittext_registration_con_password);
+        go_login_from_Reg = findViewById(R.id.textView_login_from_registration);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        firebaseAuth = FirebaseAuth.getInstance();
+
         buttonregistration.setOnClickListener(this);
+        go_login_from_Reg.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Registration Successfully", Toast.LENGTH_SHORT).show();
+
+        if (v.getId() == R.id.textView_login_from_registration) {
+            Intent intent = new Intent(Registration.this, Login.class);
+            startActivity(intent);
+
+        } else {
+            userRegistration();
+        }
+
     }
+
+    String first_name, last_name, user_email, user_password, user_con_password;
+
+    public void userRegistration() {
+
+        first_name = f_name.getText().toString().trim();
+        last_name = l_name.getText().toString().trim();
+        user_email = u_email.getText().toString().trim();
+        user_password = u_password.getText().toString().trim();
+        user_con_password = u_con_password.getText().toString().trim();
+
+        userRegistrationValidation();
+
+        firebaseAuth.createUserWithEmailAndPassword(user_email, user_password)
+                .addOnCompleteListener(Registration.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            User user = new User(first_name, last_name, user_email);
+                            databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(Registration.this, "Registration complete", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(Registration.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+
+    }
+
+    public void userRegistrationValidation() {
+
+        if (first_name.isEmpty()) {
+            f_name.setError("Enter a First name please");
+            f_name.requestFocus();
+            return;
+        }
+
+        if (last_name.isEmpty()) {
+
+            l_name.setError("Enter a Last name please");
+            l_name.requestFocus();
+            return;
+
+        }
+
+        if (user_email.isEmpty()) {
+
+            u_email.setError("Enter a Email please");
+            u_email.requestFocus();
+            return;
+
+        }
+
+        if (user_password.isEmpty()) {
+
+            u_password.setError("Enter a Password please");
+            u_password.requestFocus();
+            return;
+
+        }
+
+        if (user_con_password.isEmpty()) {
+
+            u_con_password.setError("Enter a Confirm Password please");
+            u_con_password.requestFocus();
+            return;
+
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(user_email).
+
+                matches()) {
+
+            u_email.setError("Enter a Valid Email please");
+            u_email.requestFocus();
+            return;
+
+        }
+
+        if (user_password.length() < 6) {
+            u_password.setError("Enter a atleast 6 digit Password please");
+            u_password.requestFocus();
+            return;
+        }
+        if (user_con_password.length() < 6) {
+            u_con_password.setError("Enter a atleast 6 digit Password please");
+            u_con_password.requestFocus();
+            return;
+        }
+        if (!user_password.equals(user_con_password)) {
+            u_password.setError("Password and Confirm Password must be same");
+            u_password.requestFocus();
+            return;
+
+        }
+
+    }
+
 }
+
